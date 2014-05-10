08d0e46d891fb7457019e967507155ebe678d457
Дмитрий Пацура <zaets28rus@gmail.com>
2014-05-10 18:08:20 +0400

    index page

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author: Patsura Dmitry <zaets28rus@gmail.com>
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Dmitry Patsura Blog - About Me</title>
+</head>
+<body>
+
+</body>
+</html>
