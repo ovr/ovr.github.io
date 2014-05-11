a7333d7abdcf7830983cd2acabebaa522519e572
Дмитрий Пацура <zaets28rus@gmail.com>
2014-05-11 05:04:35 +0400

    Google track code - fix

--- public/index.php
+++ public/index.php
@@ -14,13 +14,14 @@
 	<link rel="stylesheet" type="text/css" href="/css/component.css" />
 	<link rel="stylesheet" type="text/css" href="/css/about.css	" />
 	<script src="/src/vendor/modernizr/modernizr.js"></script>
-	<script type="text/javascript">
-		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-		try {
-			var pageTracker = _gat._getTracker("UA-20503308-1");
-			pageTracker._trackPageview();
-		} catch(err) {}
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-20503308-1', 'dmtry.me');
+		ga('send', 'pageview');
 	</script>
 </head>
 <body>
