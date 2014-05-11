fb434cadeeee924d74e06460452fe95aaea88f1a
Дмитрий Пацура <zaets28rus@gmail.com>
2014-05-11 14:15:36 +0400

    Статья - добавил блок коментария

--- app/article.php
+++ app/article.php
@@ -53,6 +53,20 @@
 		<?php
 			echo file_get_contents(__DIR__ . '/data/cache/phalcon_It_is_not_the_best.html');
 		?>
+		<div id="disqus_thread"></div>
+		<script type="text/javascript">
+			/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+			var disqus_shortname = 'dmtry'; // required: replace example with your forum shortname
+
+			/* * * DON'T EDIT BELOW THIS LINE * * */
+			(function() {
+				var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+				dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+			})();
+		</script>
+		<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+		<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
 	</div>
 	<script src="/src/lib/classie.js"></script>
 	<script src="/src/lib/gnmenu.js"></script>
