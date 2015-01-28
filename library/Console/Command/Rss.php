d854f3a6d96fe6c9329b2ba61da89dbff582c742
Дмитрий Пацура <talk@dmtry.me>
2015-01-28 18:46:05 +0900

    Write RSS Command

--- library/Console/Command/Rss.php
+++ library/Console/Command/Rss.php
@@ -8,6 +8,7 @@ namespace App\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Zend\Feed\Writer\Feed;
 
 /**
  * Class Rss
@@ -17,8 +18,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class Rss extends Command
 {
-    const RU_LANG = 1;
-    const EN_LANG = 2;
+    const RU_LANG = 'ru';
+    const EN_LANG = 'en';
 
     const RSS_AUTHOR = 'author';
     const RSS_TITLE = 'title';
@@ -53,5 +54,18 @@ class Rss extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln("Start generating rss");
+
+
+        foreach ($this->details as $lang => $options) {
+            $feed = new Feed;
+
+            $feed->setTitle($options[self::RSS_TITLE]);
+            $feed->setLink($options[self::RSS_LINK]);
+            $feed->setFeedLink($options[self::RSS_LINK] . 'rss/' . $lang . '-atom.xml', 'atom');
+            $feed->setDescription('My blog');
+
+            $feed->addAuthor($options[self::RSS_AUTHOR]);
+
+        }
     }
 }
